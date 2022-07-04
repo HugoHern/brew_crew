f53bdeeb1dd2de099a0fe0bfd8a2f12d3e7cb5fd
HugoHern <hugo8954@gmail.com>
2022-07-04 18:06:25 -0400

    added simple custom routing for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
 import Navbar from './Navbar';
+import Pricing from './pages/Pricing';
+import Home from './pages/Home'
+import About from './pages/About';
+import './styles.css'
 
 function App() {
-  return <Navbar/>;
+  //Simple routing
+  let component
+  switch (window.location.pathname) {
+    case '/':
+      component = <Home />
+      break
+    case '/pricing':
+      component = <Pricing />
+      break
+    case '/about':
+      component = <About />
+      break
+  }
+  return (
+    <>
+      <Navbar />
+      <div className='container'>{component}</div>
+      
+    </>
+  )
 }
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,24 @@
 //Navbar component to navigate through the website
 
-export default function Navbar(){
+export default function Navbar() {
+    
     return <nav className="nav">
-         <a href="/" className="site-title">BREW CREW</a>
-         <ul>
-             <li>
-                 <a href="/pricing">PRICING</a>
-                 <a href="/about">ABOUT</a>
-             </li>
-         </ul>
+        <a href="/" className="site-title">BREW CREW</a>
+        <ul>
+           <CustomLink href='/pricing'>PRICING</CustomLink>
+           <CustomLink href='/about'>ABOUT</CustomLink>
+        </ul>
     </nav>
+}
+
+//custom component to keep track of active page
+function CustomLink ({href, children, ...props}){
+    //keep track of active page
+    const path = window.location.pathname
+    
+    return(
+        <li className={path === href ? 'active' : ''}>
+            <a href={href} {...props}>{children}</a>
+        </li>
+    )
 }
